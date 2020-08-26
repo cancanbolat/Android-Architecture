7aa87c941478ae18d28baeee04fba2105f21de65
= <=>
2020-08-26 14:50:27 +0300

    implements MainNavigator and import AlertDialog message

--- app/src/main/java/com/example/databinding_2/MainActivity.java
+++ app/src/main/java/com/example/databinding_2/MainActivity.java
@@ -5,11 +5,12 @@ import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import com.example.databinding_2.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainNavigator {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 .of(this)
                 .get(MainViewModel.class);
 
+        viewModel.setNavigator(this);
         viewModel.setUserMutable();
         viewModel.getUserMutable().observe(this, user -> {
             mainBinding.recyclerView.setAdapter(new MainAdapter(user, viewModel));
@@ -30,4 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onItemClick(User user) {
+        new AlertDialog.Builder(this).setMessage(user.name + "\n" + user.email).show();
+    }
+
 }
